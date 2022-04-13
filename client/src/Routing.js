05244b8078979ee7526eccbfa9287ff60e9dfb20
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-04-13 23:37:46 +0530

    Routing for the app

---
+++ client/src/Routing.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./Home";
+import HA from "./Disease Components/HA";
+import BC from "./Disease Components/BC";
+import CK from "./Disease Components/CK";
+import AL from "./Disease Components/AL";
+
+import "./Routing.css";
+
+const Routing = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/HeartAttack" element={<HA />} />
+        <Route exact path="/BreastCancer" element={<BC />} />
+        <Route exact path="/ChronicKidney" element={<CK />} />
+        <Route exact path="/AcuteLiver" element={<AL />} />
+        <Route exact path="/*" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Routing;
